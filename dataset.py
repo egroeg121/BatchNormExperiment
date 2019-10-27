1653989007da071f2010451e1f8644635158a51b
George <george.barnett.121@gmail.com>
2019-10-27 20:31:14 +0000

    Basic Training

--- dataset.py
+++ dataset.py
@@ -1,43 +1,62 @@
 import torchvision
 import torch
+import torchvision.transforms as transforms
 import logging
 
 class loaders():
-    def __init__(self,transforms,data_dir = './data',num_workers = 0,batch_size=1):
-
+    def __init__(self,data_dir = './data',num_workers = 0,batch_size=1):
+        self.data_dir = data_dir
         self.batch_size = batch_size
         self.num_workers = num_workers
+        self.transforms = transforms
 
-        self.trainset  = torchvision.datasets.CIFAR10(root=data_dir,
-                                                      train=True,
-                                                      download=True,
-                                                      transform=transforms
-                                                      )
-        self.testset = torchvision.datasets.CIFAR10(root=data_dir,
-                                                    train=False,
-                                                    download=True,
-                                                    transform=transforms
-                                                    )
+    def train_loader(self):
+        transforms = self.get_transforms(mode='train')
+
+        train_set = torchvision.datasets.CIFAR10(
+            root=self.data_dir,
+            train=True,
+            download=True,
+            transform=transforms,)
+
+        return torch.utils.data.DataLoader(
+            train_set,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,)
 
 
-    def get_loader(self,mode):
+    def test_loader(self):
+        transforms = self.get_transforms(mode='test')
+
+        test_set = torchvision.datasets.CIFAR10(
+            root=self.data_dir,
+            train=False,
+            download=True,
+            transform=transforms)
+
+        return torch.utils.data.DataLoader(
+            test_set,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,)
+
+
+
+    def get_transforms(self,mode):
+        transform_list = []
+
+        # Transforms for special modes
         if mode=='train':
-            return self.train_loader(self.batch_size,self.num_workers)
+            pass
         elif mode=='test':
-            return self.test_loader()
-        else:
-            print("Incorrect Mode")
-            exit()
+            pass
+        elif mode=='val':
+            pass
 
-    def train_loader(self,batch_size,num_workers):
-        return torch.utils.data.DataLoader(self.trainset,
-                                           batch_size=batch_size,
-                                           num_workers=num_workers,
-                                           shuffle=True,
-                                           )
-    def test(self,batch_size,num_workers):
-        return torch.utils.data.DataLoader(self.testset,
-                                           batch_size=batch_size,
-                                           num_workers=num_workers,
-                                           shuffle=False,
-                                           )
+        # Default Transforms
+        transform_list.extend([
+            transforms.transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),])
+
+        return transforms.Compose(transform_list)
--- train.py
+++ train.py
@@ -1,7 +1,45 @@
 import torch
+from torch import nn
 import numpy as np
 import torchvision
 import argparse
 
-def main(args):
-    
+from models import BasicConvNet
+import dataset
+
+def main():
+    loaders = dataset.loaders(num_workers=2,batch_size=2)
+
+    model = BasicConvNet()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    num_epoch = 10
+
+    for epoch in range(num_epoch):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(loaders.train_loader(), 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+
+
+    print('Finished Training')
+
+
+def train_loop():
+    pass
+
+
+if __name__ == "__main__":
+    main()
